1867a627778525d7332c4030a119f575fe271f54
proddyt <proddyt@PRODDYT-2U6QQLO>
2026-03-17 17:50:34 -0300

    adição do milestone 2

--- missaoaurora/Form1.Designer.cs
+++ missaoaurora/Form1.Designer.cs
@@ -29,26 +29,62 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            numPotencia = new NumericUpDown();
+            btAceletar = new Button();
+            labValue = new Label();
+            ((System.ComponentModel.ISupportInitialize)numPotencia).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(206, 202);
+            label1.Location = new Point(175, 32);
             label1.Name = "label1";
             label1.Size = new Size(399, 37);
             label1.TabIndex = 0;
             label1.Text = "SISTEMA DE BORDO-AURORA";
+            // 
+            // numPotencia
+            // 
+            numPotencia.Location = new Point(118, 238);
+            numPotencia.Name = "numPotencia";
+            numPotencia.Size = new Size(120, 23);
+            numPotencia.TabIndex = 1;
+            numPotencia.ValueChanged += numPotencia_ValueChanged;
+            // 
+            // btAceletar
+            // 
+            btAceletar.Location = new Point(308, 238);
+            btAceletar.Name = "btAceletar";
+            btAceletar.Size = new Size(75, 23);
+            btAceletar.TabIndex = 2;
+            btAceletar.Text = "&ACELERAR";
+            btAceletar.UseVisualStyleBackColor = true;
+            btAceletar.Click += btAceletar_Click;
+            // 
+            // labValue
+            // 
+            labValue.AutoSize = true;
+            labValue.Location = new Point(449, 242);
+            labValue.Name = "labValue";
+            labValue.Size = new Size(38, 15);
+            labValue.TabIndex = 3;
+            labValue.Text = "label2";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(labValue);
+            Controls.Add(btAceletar);
+            Controls.Add(numPotencia);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numPotencia).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +92,8 @@
         #endregion
 
         private Label label1;
+        private NumericUpDown numPotencia;
+        private Button btAceletar;
+        private Label labValue;
     }
 }
--- missaoaurora/obj/Debug/net10.0-windows/missaoaurora.AssemblyInfo.cs
+++ missaoaurora/obj/Debug/net10.0-windows/missaoaurora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("missaoaurora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92a6ad41051bb4203d37e836ea95d97ef8c2540")]
 [assembly: System.Reflection.AssemblyProductAttribute("missaoaurora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("missaoaurora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
